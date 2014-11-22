72070e7bbc72fc2832b6b636cbcb026dfa47a8b1
Sune Gynthersen <sune.gynthersen@gmail.com>
2014-11-22 13:11:19 +0100

    Added internal and sepa transfer example

--- hello/views.py
+++ hello/views.py
@@ -1,12 +1,50 @@
+import requests
+import json
+import time
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Greeting
+from fidor import FidorClient
 
-# Create your views here.
+
+access_token = 'bb0c4de1ec6fda142d48991995f0dbaa'
+api_url = 'https://aps.fidor.de'
+
 def index(request):
-    return HttpResponse('Hello from Python!')
+    fidor = FidorClient(access_token, api_url)
+    account = fidor.accounts.get(8)
+    
+    return render(request, 'index.html', {'account': account})
 
+def transfer(request):
+    fidor = FidorClient(access_token, api_url)
+
+    #payload = {
+    #    'account_id': 8,
+    #    'amount': 1,
+    #    'external_uid': int(time.time()),
+    #    'receiver': '99996807',
+    #    'currency': 'EUR',
+    #    'subject': 'Tak for kaffe!'
+    #}
+
+    payload = {
+        'account_id': 8,
+        'amount': 1,
+        'remote_iban': 'DE04333706726265131076',
+        'external_uid': int(time.time()),
+        'subject': 'Tak for kaffe!'
+    }
+
+
+
+    print fidor.sepa_credit_transfers.create(payload)
+    account = fidor.accounts.get(8)
+
+
+    return render(request,'transfer.html', {'account': account})
 
 def db(request):
 
